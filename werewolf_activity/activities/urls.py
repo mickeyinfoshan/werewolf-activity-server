ce03baaacd8bea6ea2e51fc53d3dacec7844e7fd
mickey <mickey@qq.com>
2017-06-13 14:55:07 +0800

    add views

--- werewolf_activity/activities/urls.py
+++ werewolf_activity/activities/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^list', views.activity_list, name='activity list'),
-    url(r'^(?P<activity_id>\d+)/$', views.activity_item, name="activity item")
+    url(r'^(?P<activity_id>\d+)/$', views.activity_item, name="activity item"),
+    url(r'^new', views.create_activity, name="create activity"),
+    url(r'^(?P<activity_id>\d+)/update', views.update_activity, name="update activity"),
+    url(r'^(?P<activity_id>\d+)/delete', views.del_activity, name="delete activity"),
+    url(r'^(?P<activity_id>\d+)/join', views.join_activity, name="join activity"),
+    url(r'^(?P<activity_id>\d+)/quit', views.quit_activity, name="quit activity"),
 ]
--- werewolf_activity/activities/views.py
+++ werewolf_activity/activities/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from .models import Activity, Participation
+import json
+from datetime import datetime
 
 
 class PageRequest:
@@ -84,7 +86,119 @@ def make_activity_response(activity):
 
 def join_activity(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
+    participant_count = activity.participation_set.count()
+    if participant_count >= activity:
+        return JsonResponse({"msg": "人数已达上限"}, status=400)
     params = {"openID": "", "avatar": "",
                        "nickname": "", "comment": ""}
-    body = 
-    for param in params:
+    json_body = {}
+    try:
+        json_body = json.loads(request.body)
+    except:
+        pass
+    param_sources = [request.GET, request.POST, json_body]
+    for key in params:
+        for source in param_sources:
+            if key in source:
+                params[key] = source[key]
+    
+    params_can_blank = ["comment"]
+    for key in params:
+        if key not in params_can_blank and params[key] == "":
+            return JsonResponse({"msg": "%s不能为空" % key}, status=400)
+    
+    has_participated = activity.participation_set.filter(user_open_id=params["openID"]).count() > 0
+    if has_participated:
+        return JsonResponse({"msg": "已参加"}, status=400)
+    activity.participation_set.create(user_open_id=params["openID"], user_avatar=params["avatar"], user_nickname=params["nickname"], comment=params["comment"])
+    return JsonResponse({"msg": "成功参加"})
+
+def quit_activity(request, activity_id):
+    params = {"openID": ""}
+    json_body = {}
+    try:
+        json_body = json.loads(request.body)
+    except:
+        pass
+    
+    param_sources = [request.GET, request.POST, json_body]
+    for key in params:
+        for source in param_sources:
+            if key in source:
+                params[key] = source[key]
+
+    params_can_blank = []
+    for key in params:
+        if key not in params_can_blank and params[key] == "":
+            return JsonResponse({"msg": "%s不能为空" % key}, status=400)
+    activity = get_object_or_404(Activity, pk=activity_id)
+    activity.participation_set.filter(user_open_id=params["openID"]).delete()
+    return JsonResponse({"msg":"成功鸽子"})
+
+def del_activity(request, activity_id):
+    activity = get_object_or_404(Activity, pk=activity_id)
+    activity.delete()
+    return JsonResponse({"msg":"删局成功"})
+
+def save_activity(request, activity):
+    str_params = {
+        "openID": "", 
+        "avatar": "",
+        "nickname": "", 
+        "name": "", 
+        "address": "", 
+        "description":"",
+    }
+    number_params = {
+        "start": 0,
+        "end": 0,
+        "price": 0,
+        "max": 15, 
+    }
+    json_body = {}
+    try:
+        json_body = json.loads(request.body)
+    except:
+        pass
+    param_sources = [request.GET, request.POST, json_body]
+    for key in str_params:
+        for source in param_sources:
+            if key in source:
+                str_params[key] = source[key]
+
+    for key in number_params:
+        for source in param_sources:
+            if key in source:
+                try:
+                    number_params[key] = int(source[key])
+                except:
+                    pass
+    
+    params_can_blank = ["description"]
+    for key in str_params:
+        if key not in params_can_blank and str_params[key] == "":
+            return JsonResponse({"msg": "%s不能为空" % key}, status=400)
+    
+    activity.name = str_params["name"]
+    activity.start = datetime.fromtimestamp(number_params["start"])
+    activity.end = datetime.fromtimestamp(number_params["end"])
+    activity.address = str_params["address"]
+    activity.creator_avatar = str_params["avatar"]
+    activity.creator_nickname = str_params["nickname"]
+    activity.creator_open_id = str_params["openID"]
+    activity.price = number_params["price"]
+    activity.max_participants = number_params["max"]
+    activity.description = str_params["description"]
+    try:
+        activity.save()
+        return JsonResponse({"msg": "保存成功"})
+    except:
+        return JsonResponse({"msg": "保存失败"}, status=500)
+
+def create_activity(request):
+    activity = Activity()
+    return save_activity(request, activity)
+
+def update_activity(request, activity_id):
+    activity = get_object_or_404(Activity, pk=activity_id)
+    return save_activity(request, activity)
